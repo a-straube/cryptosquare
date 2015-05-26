651780b0e3c002aa6272570af26a51aedaf8c01b
jordan & axel <student@epicodus.com>
2015-05-26 16:46:20 -0700

    make a sentence all lowercase

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,15 @@ var cryptoSquare = function(message) {
  message = message.toLowerCase();
  return message;
 };
+
+$(document).ready(function() {
+  $("form#cryptoMessage").submit(function(event) {
+    var message = $("input#message").val();
+    var length = message.length()
+    var squareRoot = length.sqrt()
+    
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
